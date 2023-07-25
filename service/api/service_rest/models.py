aca794fb1260824af59c7d766faa91f3c152dcf3
Kathy Gomez <gomezkathy1997@gmail.com>
2023-07-24 17:53:22 -0700

    first commit

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Technician(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.first_name
+
+class AutomobileVO(models.Model):
+    color = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+    sold = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.vin
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.TextField()
+    status = models.CharField(max_length=100)
+    vin = models.CharField(max_length=17, unique=True)
+    customer_name = models.CharField(max_length=200)
+
+    technician = models.ForeignKey(
+        Technician,
+        related_name = "appointment",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.customer_name
--- service/poll/poller.py
+++ service/poll/poller.py
@@ -12,6 +12,8 @@ django.setup()
 # Import models from service_rest, here. Ignore vs-code error hinting
 # from service_rest.models import Something
 
+from service_rest.models import AutomobileVO
+
 
 def poll(repeat=True):
     while True:
@@ -19,8 +21,21 @@ def poll(repeat=True):
         try:
             # Write your polling logic, here
             # Do not copy entire file
-            pass
-        
+
+            response = requests.get('http://inventory-api:8000/api/automobiles/')
+            content = json.loads(response.content)
+
+            for automobile in content["auto"]:
+                AutomobileVO.objects.update_or_create(
+                    import_href=automobile["href"],
+                    defaults ={
+                        "color":automobile["color"],
+                        "year": automobile["color"],
+                        "vin": automobile["vin"],
+                    }
+
+                )
+
         except Exception as e:
             print(e, file=sys.stderr)
 
