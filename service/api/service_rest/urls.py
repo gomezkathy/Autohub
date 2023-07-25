94ca42bd1556dc878ea18ae3bc574c667a745e53
Kathy Gomez <gomezkathy1997@gmail.com>
2023-07-25 11:01:44 -0700

    working technician list,create,delete

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -1,1 +1,6 @@
 from django.urls import path
+from .views import api_technician_list, api_technician_detail
+urlpatterns = [
+    path("technicians/", api_technician_list, name="technician_list"),
+    path("technicians/<int:pk>/", api_technician_detail, name= "technician_detail"),
+]
