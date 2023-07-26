3066c5b6f2e7a88655ac95dc3114c89f548b6e73
Kathy Gomez <gomezkathy1997@gmail.com>
2023-07-26 10:31:52 -0700

    working technician form

--- ghi/app/src/TechnicianForm.js
+++ ghi/app/src/TechnicianForm.js
@@ -1,7 +1,73 @@
 import React, { useEffect, useState } from 'react';
 
 function TechnicianForm() {
+    const[values, setValues] = useState([]);
+    const[firstName, setFirstName] = useState('');
+    const[lastName, setLastName] = useState('');
+    const[employeeID, setEmployee] = useState('');
 
+    const handleFirstNameChange = (event) => {
+        const value = event.target.value;
+        setFirstName(value);
+    }
+
+    const handleLastNameChange = (event) => {
+        const value = event.target.value;
+        setLastName(value);
+    }
+
+    const handleEmployeeChange = (event) => {
+        const value = event.target.value;
+        setEmployee(value);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = {};
+
+        data.first_name = firstName;
+        data.last_name = lastName;
+        data.employee_id = employeeID;
+
+        console.log(data);
+
+        const technicianUrl = 'http://localhost:8080/api/technicians/';
+        const fetchConfig = {
+          method: "post",
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        };
+
+        const response = await fetch(technicianUrl, fetchConfig);
+        if (response.ok) {
+          const newTechnician = await response.json();
+          console.log(newTechnician);
+
+          setFirstName('');
+          setLastName('');
+          setEmployee('');
+
+        }
+
+    }
+
+
+    const fetchData = async () => {
+        const url = 'http://localhost:8100/api/automobiles/';
+
+        const response = await fetch(url);
+
+        if (response.ok) {
+          const data = await response.json();
+          setValues(data.values);
+        }
+      }
+
+      useEffect(() => {
+        fetchData();
+      }, []);
 
 
     return (
@@ -9,17 +75,17 @@ function TechnicianForm() {
         <div className="offset-3 col-6">
           <div className="shadow p-4 mt-4">
             <h1>Add a Technician</h1>
-            <form id="create-technician-form">
+            <form  onSubmit={handleSubmit} id="create-technician-form">
               <div className="form-floating mb-3">
-                <input placeholder="First Name" required type="text" name="first_name" id="first_name" className="form-control" />
+                <input value={firstName} onChange={handleFirstNameChange} placeholder="First Name" required type="text" name="first_name" id="first_name" className="form-control" />
                 <label htmlFor="first_name">First Name</label>
               </div>
               <div className="form-floating mb-3">
-                <input placeholder="Last Name" required type="text" name="last_name" id="last_name" className="form-control" />
+                <input value={lastName} onChange={handleLastNameChange} placeholder="Last Name" required type="text" name="last_name" id="last_name" className="form-control" />
                 <label htmlFor="last_namet">Last Name</label>
               </div>
               <div className="form-floating mb-3">
-                <input placeholder="Employee ID" required type="text" name="employee_id" id="employee_id" className="form-control"/>
+                <input value={employeeID} onChange={handleEmployeeChange} placeholder="Employee ID" required type="text" name="employee_id" id="employee_id" className="form-control"/>
                 <label htmlFor="employee_id">Employee ID</label>
               </div>
               <button className="btn btn-primary">Create</button>
