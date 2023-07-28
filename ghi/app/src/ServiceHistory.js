4e852723efe3f22914993d37df42e3a9dc1ae8b8
Kathy Gomez <gomezkathy1997@gmail.com>
2023-07-27 23:58:26 -0700

    search bar done

--- ghi/app/src/ServiceHistory.js
+++ ghi/app/src/ServiceHistory.js
@@ -1,12 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
-function ServiceHistory(props){
-
+function ServiceHistory(props, appointments) {
     const [auto, setAuto] = useState([]);
+    const [searchVIN, setSearchVIN] = useState('');
+    const [filteredAppointments, setFilteredAppointments] = useState([]);
 
-    async function getAutoData(){
+    const handleApptChange = (event) => {
+        const value = event.target.value;
+        setSearchVIN(value);
+      };
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        const data = props.appointments.filter(
+            appointment => appointment.vin.toLowerCase() === searchVIN.toLowerCase()
+            );
+            setFilteredAppointments(data);
+        };
+
+    async function getAutoData() {
         const response = await fetch('http://localhost:8100/api/automobiles/');
-        if(response.ok){
+        if (response.ok) {
             const data = await response.json();
             setAuto(data.autos);
         }
@@ -14,53 +28,63 @@ function ServiceHistory(props){
 
     useEffect(() => {
         getAutoData();
-    })
+    }, []);
 
     function isVip(vin) {
-        return auto.some((item)=> item.vin ===vin);
+        return auto.some((item) => item.vin === vin);
     }
 
-    if (props.appointments === undefined){
+    if (props.appointments === undefined) {
         return null;
     }
 
-    return(
+
+    if (filteredAppointments.length > 0) {
+        appointments = filteredAppointments;
+    } else {
+        appointments = props.appointments;
+    }
+
+    return (
         <div>
             <h2>Service History</h2>
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>VIN</th>
-                    <th>Is VIP?</th>
-                    <th>Customer</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Technician</th>
-                    <th>Reason</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.appointments.map (appointment =>{
-                    return(
+            <form onSubmit={handleSearch} className="create-search-form">
+                <div className="form-floating mb-3">
+                    <input value={searchVIN} onChange={handleApptChange} placeholder="VIN Search" required type ="text" name="search_vin" id="search_vin" className="form-control" />
+                    <label htmlFor="search_vin">Search by VIN</label>
+                    <button type="submit">Search</button>
+                </div>
+            </form>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>VIN</th>
+                        <th>Is VIP?</th>
+                        <th>Customer</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                        <th>Technician</th>
+                        <th>Reason</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {appointments.map((appointment) => (
                         <tr key={appointment.id}>
                             <td>{appointment.vin}</td>
-                            <td>{ isVip(appointment.vin) ? 'Yes': 'No'}</td>
+                            <td>{isVip(appointment.vin) ? 'Yes' : 'No'}</td>
                             <td>{appointment.customer_name}</td>
                             <td>{new Date(appointment.date_time).toLocaleDateString()}</td>
                             <td>{new Date(appointment.date_time).toLocaleTimeString()}</td>
                             <td>{appointment.technician}</td>
                             <td>{appointment.reason}</td>
                             <td>{appointment.status}</td>
-
                         </tr>
-                    )
-                }
-                    )}
-            </tbody>
-        </table>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 
 export default ServiceHistory;
